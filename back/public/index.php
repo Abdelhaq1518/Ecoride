2324b0f056ce00f451b2f91170079afdccd9097d
Abdel Ait Said <aaitsaid78@gmail.com>
2025-06-22 19:32:02 +0200

    Ajout du H1 et de la section de présentation

--- back/public/index.php
+++ back/public/index.php
@@ -1,4 +1,24 @@
 <?php include '../includes/header.php'; ?>
+<main>
+  <h1 class="text-center mt-5">EcoRide</h1>
+
+  <section class="presentation-searchbar container-fluid min-vh-100 d-flex flex-column flex-md-row">
+    <!-- Partie gauche -->
+    <div class="col-md-6 d-flex flex-column justify-content-center p-4">
+      <h2 class="mb-4">C'est quoi EcoRide ?</h2>
+      <p>
+        EcoRide est une start-up en pleine expansion qui propose une
+        plateforme de covoiturage respectueuse de l’environnement.
+        Spécialisée dans les trajets en voiture — avec une priorité donnée
+        aux véhicules électriques, devant les thermiques et hybrides —
+        EcoRide offre à ses utilisateurs la possibilité d’être passager,
+        conducteur ou les deux. Les trajets permettent de gagner des crédits
+        via une monnaie virtuelle. Innovation, partage et réduction de
+        l’empreinte carbone sont les piliers d’EcoRide. <br /><br />
+        Avec EcoRide,
+        <strong>“on peut aller loin en se mettant au vert”.</strong>
+      </p>
+    </div>
 
 
 <?php include '../includes/footer.php'; ?>
